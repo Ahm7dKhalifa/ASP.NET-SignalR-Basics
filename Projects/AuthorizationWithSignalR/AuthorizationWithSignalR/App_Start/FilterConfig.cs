ef4bf65c395db9b18b92a41b137a854417eb5771
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2019-01-08 00:53:23 +0200

    ADD AuthorizationWithSignalR Folder IN Projects Folder

---
+++ Projects/AuthorizationWithSignalR/AuthorizationWithSignalR/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AuthorizationWithSignalR
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
