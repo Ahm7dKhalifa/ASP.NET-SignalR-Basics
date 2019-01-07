a980a7bcbff1572382007d2e9b08f6be2104b9ae
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2019-01-08 01:04:49 +0200

    ADD CallingHubFromController Folder IN Projects Folder

---
+++ Projects/CallingHubFromController/CallingHubFromController/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using CallingHubFromController.SignalR_Backend;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CallingHubFromController.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SendNotifications()
+        {
+            return View();
+        }
+
+        public PartialViewResult CallingHub()
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<Notifications>();
+            hubContext.Clients.All.addNewMessageToPage("you have receive notification on " + DateTime.Now);
+            return PartialView();
+        }
+
+        public ActionResult recivedNotification()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Projects/CallingHubFromController/CallingHubFromController/SignalR_Backend/Notifications.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace CallingHubFromController.SignalR_Backend
+{
+    public class Notifications : Hub
+    {
+
+    }
+}
