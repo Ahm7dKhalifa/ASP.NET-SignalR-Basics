e0d0b0a399e48d9d8149abf7b6c60f20bb857a46
ahm7dkhalifa <ahm7dkhalifa@gmail.com>
2019-03-23 18:26:59 +0200

    restructured and organized the folders

---
+++ _Projects/AuthorizationWithSignalR/AuthorizationWithSignalR/SignalR_Hubs/ChatHub.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace AuthorizationWithSignalR.SignalR_Hubs
+{
+    [Authorize(Roles = "Employee")]
+    public class ChatHub : Hub
+    {
+
+        private readonly static ConnectionMapping<string> _connections = new ConnectionMapping<string>();
+
+        public void Send(string who,string message)
+        {
+            //who  ==> reciver user
+            //name ==> current user or sender user
+            string name = Context.User.Identity.Name;
+
+            if(who == "All")
+            {
+                Clients.All.addChatMessage(name + ": " + message);
+            }
+            else
+            {
+                foreach (var connectionId in _connections.GetConnections(who))
+                {
+                    Clients.Client(connectionId).addChatMessage(name + ": " + message);
+                }
+            }   
+        }
+
+        public override Task OnConnected()
+        {
+            string name = Context.User.Identity.Name;
+
+            _connections.Add(name, Context.ConnectionId);
+
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            string name = Context.User.Identity.Name;
+
+            _connections.Remove(name, Context.ConnectionId);
+
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            string name = Context.User.Identity.Name;
+
+            if (!_connections.GetConnections(name).Contains(Context.ConnectionId))
+            {
+                _connections.Add(name, Context.ConnectionId);
+            }
+
+            return base.OnReconnected();
+        }
+
+        
+    }
+}
